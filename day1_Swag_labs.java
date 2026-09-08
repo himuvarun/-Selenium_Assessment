3ffe7f5decb3fe7930c548c704b014cfda6cf070
himuvarun <167072408+himuvarun@users.noreply.github.com>
2026-09-08 09:39:22 +0530

    Add files via upload

---
+++ day1_Swag_labs.java
@@ -0,0 +1,44 @@
+package mouseAction;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+import org.openqa.selenium.io.FileHandler;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class day1_Swag_labs {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriver driver=new EdgeDriver();
+		driver.manage().window().maximize();	
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://www.saucedemo.com/");
+		WebElement name=driver.findElement(By.id("user-name"));
+		name.sendKeys("standard_user");
+		
+		WebElement password=driver.findElement(By.id("password"));
+		password.sendKeys("secret_sauce");
+		
+		WebElement btn=driver.findElement(By.id("login-button"));
+		btn.click();
+		
+		JavascriptExecutor js=(JavascriptExecutor) driver;
+		js.executeScript("window.scrollTo(0,500)");
+		Thread.sleep(2000);
+		
+		TakesScreenshot tks=(TakesScreenshot) driver;
+		File src=tks.getScreenshotAs(OutputType.FILE);
+		File dest=new File("C:\\Users\\Admin\\Desktop\\javacapgemini\\New folder\\products-page.png");
+		FileHandler.copy(src, dest);
+		
+	}
+
+}
