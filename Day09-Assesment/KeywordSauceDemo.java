fe3f5a3b37bf485d5d831a6983eaac5fb7df972e
himuvarun <167072408+himuvarun@users.noreply.github.com>
2026-09-17 09:33:36 +0530

    Add files via upload

---
+++ Day09-Assesment/KeywordSauceDemo.java
@@ -0,0 +1,18 @@
+package Day9;
+import Day9.KeywordExecutorSauceDemo;
+import Day9.KeywordImplementaionSauceDemo;
+
+public class KeywordSauceDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		KeywordExecutorSauceDemo e=new KeywordExecutorSauceDemo();
+	    e.executor("OPEN_BROWSER");
+	    e.executor("OPEN_URL");
+	    e.executor("USERNAME");
+	    e.executor("PASSWORD");
+	    e.executor("LOGIN");
+	    e.executor("CLOSE_BROWSER");
+	}
+
+}
